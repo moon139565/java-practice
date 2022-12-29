060d36854c3feb586e72002f4b641102ac5a7951
moon1395695 <anika01839@gmail.com>
2022-12-29 11:30:16 +0600

    class new

--- script.js
+++ script.js
@@ -1,160 +1,160 @@
-let firstName = "arif";
-let age = 33;
-console.log(firstName);
-console.log(age);
+// let firstName = "arif";
+// let age = 33;
+// console.log(firstName);
+// console.log(age);
 
-// a. Object
-// b. premitive
+// // a. Object
+// // b. premitive
 
-// b. Premitive
-// 1. Number
-// 2. String
-// 3. Boolean
-// 4. Undefined
-// 5. Null
+// // b. Premitive
+// // 1. Number
+// // 2. String
+// // 3. Boolean
+// // 4. Undefined
+// // 5. Null
 
-// 6. Symble
-// 7. BigInit
+// // 6. Symble
+// // 7. BigInit
 
-//1. Number: It will show number
-let day = 28;
-console.log(day);
+// //1. Number: It will show number
+// let day = 28;
+// console.log(day);
 
-// 2. String: which one should be write on double quotation, but quotation patern must be same in whole website.
-let lastName = "akondo";
-console.log(lastName);
+// // 2. String: which one should be write on double quotation, but quotation patern must be same in whole website.
+// let lastName = "akondo";
+// console.log(lastName);
 
-// 3. Boolean: its mean true or false
-let isSinger = true;
-console.log(isSinger);
+// // 3. Boolean: its mean true or false
+// let isSinger = true;
+// console.log(isSinger);
 
-// 4. Undefined: The value will define later, data is not selected. its mean Undefined
-let homeCity;
-console.log(homeCity);
+// // 4. Undefined: The value will define later, data is not selected. its mean Undefined
+// let homeCity;
+// console.log(homeCity);
 
-// 5. Null: Its mean nothing.
-let income = null;
-console.log(income);
+// // 5. Null: Its mean nothing.
+// let income = null;
+// console.log(income);
 
-// How to know data type
-console.log(age);
-console.log(typeof age);
-console.log(typeof homeCity);
-console.log(typeof isSinger);
+// // How to know data type
+// console.log(age);
+// console.log(typeof age);
+// console.log(typeof homeCity);
+// console.log(typeof isSinger);
 
-// undefined data assign
-console.log(homeCity);
-homeCity = "Dhaka";
-console.log(homeCity);
+// // undefined data assign
+// console.log(homeCity);
+// homeCity = "Dhaka";
+// console.log(homeCity);
 
-// Boolean to string
-console.log(isSinger);
-isSinger = "Bangladeshi Singer";
-console.log(isSinger);
-console.log(typeof isSinger);
+// // Boolean to string
+// console.log(isSinger);
+// isSinger = "Bangladeshi Singer";
+// console.log(isSinger);
+// console.log(typeof isSinger);
 
-// bug(object): its not object its bug
-console.log(typeof income);
+// // bug(object): its not object its bug
+// console.log(typeof income);
 
-// vairable declertation
-/*
-1. let
-2. const
-3. var
-*/
-// 1. let: For reassign data we will use let
-let myAge = 33;
-console.log(myAge);
-myAge = 35;
-console.log(myAge);
+// // vairable declertation
+// /*
+// 1. let
+// 2. const
+// 3. var
+// */
+// // 1. let: For reassign data we will use let
+// let myAge = 33;
+// console.log(myAge);
+// myAge = 35;
+// console.log(myAge);
 
-// 2. Const: which date will be not change or reassign. will try to use it maximum time
-const birthyear = 1988;
-console.log(birthyear);
+// // 2. Const: which date will be not change or reassign. will try to use it maximum time
+// const birthyear = 1988;
+// console.log(birthyear);
 
-// 3. var: we will not use var, its almost similler to let. its mainly use for Global Scope, Function Scope,
-var job = "nothing";
-console.log(job);
+// // 3. var: we will not use var, its almost similler to let. its mainly use for Global Scope, Function Scope,
+// var job = "nothing";
+// console.log(job);
 
-// JavaScript Operators
-// 1. Arithmetic Operators
-console.log(18 - 12);
-let profit = 32 - 28;
-console.log(profit);
+// // JavaScript Operators
+// // 1. Arithmetic Operators
+// console.log(18 - 12);
+// let profit = 32 - 28;
+// console.log(profit);
 
-let arifAge = 2022 - 1987;
-console.log(arifAge);
+// let arifAge = 2022 - 1987;
+// console.log(arifAge);
 
-let adritaAge = 2022 - 2017;
-console.log(adritaAge);
+// let adritaAge = 2022 - 2017;
+// console.log(adritaAge);
 
-// for short code
-let currentYear = 2022;
+// // for short code
+// let currentYear = 2022;
 
-let onikAge = currentYear - 1987;
-console.log(onikAge);
+// let onikAge = currentYear - 1987;
+// console.log(onikAge);
 
-let nolaAge = currentYear - 2020;
-console.log(nolaAge);
+// let nolaAge = currentYear - 2020;
+// console.log(nolaAge);
 
-// assign two variable in oneline
-console.log(onikAge, nolaAge);
+// // assign two variable in oneline
+// console.log(onikAge, nolaAge);
 
-// age diff..
-let ageDiff = onikAge - nolaAge;
-console.log(ageDiff);
+// // age diff..
+// let ageDiff = onikAge - nolaAge;
+// console.log(ageDiff);
 
-// Arithmetic Operators
-// +
-console.log(10 + 5);
-//  -
-console.log(10 - 5);
-//  *
-console.log(10 * 5);
-// /
-console.log(10 / 5);
-// **
-console.log(2 ** 5);
-// %
-console.log(25 % 3);
-// ++
-let x = 10;
-x++;
-console.log(x);
-// --
-let y = 20;
-y--;
-console.log(y);
+// // Arithmetic Operators
+// // +
+// console.log(10 + 5);
+// //  -
+// console.log(10 - 5);
+// //  *
+// console.log(10 * 5);
+// // /
+// console.log(10 / 5);
+// // **
+// console.log(2 ** 5);
+// // %
+// console.log(25 % 3);
+// // ++
+// let x = 10;
+// x++;
+// console.log(x);
+// // --
+// let y = 20;
+// y--;
+// console.log(y);
 
-console.log(100 - 50 + (40 * 20) / 2);
+// console.log(100 - 50 + (40 * 20) / 2);
 
-let goodName = "arif";
-let surName = "akondo";
-console.log(goodName + " " + surName);
+// let goodName = "arif";
+// let surName = "akondo";
+// console.log(goodName + " " + surName);
 
-// Assignment Operators
-//=
-let item = 10 + 5;
-console.log(item);
-// +=
-let b = 15;
-b += 10; /*b = b + 10*/
-console.log(b);
+// // Assignment Operators
+// //=
+// let item = 10 + 5;
+// console.log(item);
+// // +=
+// let b = 15;
+// b += 10; /*b = b + 10*/
+// console.log(b);
 
-// -/
-let c = 20;
-c -= 15; /*= c -15*/
-console.log(c);
+// // -/
+// let c = 20;
+// c -= 15; /*= c -15*/
+// console.log(c);
 
-// *=
-let z = 5;
-z *= 5; /*z= z * 5*  so {5*5=25}*/
-console.log(z);
+// // *=
+// let z = 5;
+// z *= 5; /*z= z * 5*  so {5*5=25}*/
+// console.log(z);
 
-// /=
-let w = 15;
-w /= 3; /*w = w / 3*/
-console.log(w);
+// // /=
+// let w = 15;
+// w /= 3; /*w = w / 3*/
+// console.log(w);
 
 
 
@@ -162,70 +162,261 @@ console.log(w);
 // class 3 started
 
 // arithic oparetor
-let a = 6;
-let b = 4;
-console.log(" a + b = ", a+b)
-console.log(" a - b = ", a-b)
-console.log(" a * b = ", a*b)
-console.log(" a / b = ", a/b)
-console.log(" a ** b = ", a**b)
-console.log(" a % b = ", a%b)
-console.log(" a++ = ", a++)
-console.log(" ++a = ", ++a)
-console.log(" a-- = ", a--)
-console.log(" --a = ", --a)
+// let a = 6;
+// let b = 4;
+// console.log(" a + b = ", a+b)
+// console.log(" a - b = ", a-b)
+// console.log(" a * b = ", a*b)
+// console.log(" a / b = ", a/b)
+// console.log(" a ** b = ", a**b)
+// console.log(" a % b = ", a%b)
+// console.log(" a++ = ", a++)
+// console.log(" ++a = ", ++a)
+// console.log(" a-- = ", a--)
+// console.log(" --a = ", --a)
 
 // asgnment oparetor
 
-let nola =1;
-console.log("nola is now =", nola)
-nola += 5;
-console.log("nola is now =", nola)
-nola -= 5;
-console.log("nola is now =", nola)
-nola *= 5;
-console.log("nola is now =", nola)
-nola /= 5;
-console.log("nola is now =", nola)
-nola %= 5;
-console.log("nola is now =", nola)
-nola **= 1;
-console.log("nola is now =", nola)
+// let nola =1;
+// console.log("nola is now =", nola)
+// nola += 5;
+// console.log("nola is now =", nola)
+// nola -= 5;
+// console.log("nola is now =", nola)
+// nola *= 5;
+// console.log("nola is now =", nola)
+// nola /= 5;
+// console.log("nola is now =", nola)
+// nola %= 5;
+// console.log("nola is now =", nola)
+// nola **= 1;
+// console.log("nola is now =", nola)
 
 // comparison oparetor
 
-let comp1 = 7;
-let comp2 = 6;
-console.log(" comp1 == comp2 is", comp1==comp2)
-console.log(" comp1 != comp2 is", comp1!=comp2)
-console.log(" comp1 === comp2 is", comp1===comp2)
-console.log(" comp1 !== comp2 is", comp1!==comp2)
-console.log(" comp1 > comp2 is", comp1>comp2)
-console.log(" comp1 < comp2 is", comp1<comp2)
-console.log(" comp1 >= comp2 is", comp1>=comp2)
-console.log(" comp1 <= comp2 is", comp1<=comp2)
+// let comp1 = 7;
+// let comp2 = 6;
+// console.log(" comp1 == comp2 is", comp1==comp2)
+// console.log(" comp1 != comp2 is", comp1!=comp2)
+// console.log(" comp1 === comp2 is", comp1===comp2)
+// console.log(" comp1 !== comp2 is", comp1!==comp2)
+// console.log(" comp1 > comp2 is", comp1>comp2)
+// console.log(" comp1 < comp2 is", comp1<comp2)
+// console.log(" comp1 >= comp2 is", comp1>=comp2)
+// console.log(" comp1 <= comp2 is", comp1<=comp2)
 
 // logical oparetor
 
-let x = 6;
-let y = 4;
-console.log(x>y && x==6)
-console.log(x<y || x==6)
-console.log(!true)
+// let x = 6;
+// let y = 4;
+// console.log(x>y && x==6)
+// console.log(x<y || x==6)
+// console.log(!true)
 
 
-// if else
 
-let a = prompt('what is your age');
-if (a < 0) {
-  alert('this is invalid age');
+
+// let name = " moon ";
+// const functionn = (name) => {
+//     // const nameee = "moon";
+//     return 25 + name;
+// }
+// const aname = functionn("pagol");
+// console.log(aname)
+
+
+
+// class 7 code 
+
+// object
+
+
+/*const userobject = {
+    firstName: " nola ",
+    lasrName: " khan ",
+    birthDay: 1998,
+    className: "Ten",
+};
+// console.log(userobject);
+// console.log(userobject["firstName"])
+// console.log(userobject.birthDay)
+
+//add 
+userobject.hobby = "song";
+console.log(userobject);
+userobject.birthPlace = "bogra";
+console.log(userobject);
+// userobject["birthPlace"] = "bogra";
+console.log(userobject["birthPlace"])
+// console.log(userobject);*/
+
+
+
+
+/*const userobjectTwo = {
+    firstName: " nola ",
+    lasrName: " khan ",
+    isjob: true,
+    calculateAge: function (birthYear) {
+        return 2022 - birthYear;
+    }
+};
+console.log(userobjectTwo);
+console.log(userobjectTwo.isjob)
+console.log(userobjectTwo.calculateAge(1998))
+console.log(userobjectTwo["calculateAge"] (2000))*/
+
+
+/*const userobjectTwo = {
+    firstName: " nola ",
+    lasrName: " khan ",
+    birthYear: 1998,
+    isjob: true,
+    calculateAge: function () {
+        console.log(this.birthYear);
+        return 2022 - this.birthYear;
+    }
 }
-else if (a>17){
-  alert('you can talk him');
-}
-else if (a>9){
-  alert('may be he can!');
-}
-else {
-  alert('no way');
-}
+console.log(userobjectTwo.calculateAge())*/
+
+// const userobjectTwo = {
+//     firstName: " nola ",
+//     lasrName: " khan ",
+//     birthYear: 1998,
+//     isjob: true,
+//     calculateAge: function () {
+//         return 2022 - this.birthYear;
+//     },
+//     userDetails: function () {
+//         return `This is user ${this.firstName} ${this.lasrName}, born in ${this.birthYear}, job ${this.isjob}`
+//     }
+// }
+// console.log(userobjectTwo.calculateAge())
+// console.log(userobjectTwo)
+
+
+
+
+// for loop
+
+// console.log("Task 1");
+// console.log("Task 2");
+// console.log("Task 3");
+// console.log("Task 4");
+// console.log("Task 5");
+// console.log("Task 6");
+// console.log("Task 7");
+// console.log("Task 8");
+// for(let task = 1; task <=8; task++){
+//     console.log(`task ${task}`)
+// }
+
+// for loop 2nd exm
+// for(let i=1; i<=200; i++){
+//     console.log(`i ${i}`)
+// }
+
+// for loop 3rd exm
+// for(let moon = 1; moon <=400; moon++){
+//     console.log(`moon ${moon}`)
+// }
+
+// exmpl real
+// const newArray =[ 
+//     "Task 1",
+//     "Task 2",
+//     "Task 3",
+//     "Task 4",
+//     "Task 5",
+//     "Task 6",
+//     "Task 7"
+// ];
+// console.log(newArray)
+
+// for (let i = 0; i <=5; i++){
+//     console.log(newArray[i]);
+// }
+// new exm
+// for (let i = 0; i < newArray.length; i++){
+//     console.log(newArray[i])
+// }
+
+// // backcode
+// for (let i = newArray.length - 1; i >= 0; i--){
+//     console.log(newArray[i])
+// }
+// loop break
+// for ( let i =0; i < newArray.length; i++){
+//     if (i == 4) break;
+//     console.log(newArray[i])
+// }
+// for ( i = 0; i < newArray.length; i++){
+//     if (i == 6) break;
+//     console.log(newArray[i])
+// }
+// loop skip
+// for ( i = 0; i < newArray.length; i++){
+//     if (i == 6) continue;
+//     console.log(newArray[i])
+// }
+
+
+
+// while loop
+// let i = 1;
+// while (i <= 10){
+//     console.log(`i ${i}`)
+//     i++
+// }
+
+// const newArray =[ 
+//     "Task 1",
+//     "Task 2",
+//     "Task 3",
+//     "Task 4",
+//     "Task 5",
+//     "Task 6",
+//     "Task 7",
+//     "Task 8",
+// ];
+// // let i = 1;
+// // while (i <= 5){
+// //     console.log(newArray[i]);
+// //     i++
+// // }
+// let i = 0;
+// while(i < newArray.length){
+//     console.log(newArray[i])
+//     i++
+// }
+// back code
+// let i = newArray.length -1;
+// while(i >= 0){
+//     console.log(newArray[i])
+//     i--
+// }
+// let i = 0;
+// while (i < newArray.length){
+//     if (i == 5) break;
+//     console.log(newArray[i])
+//     i++
+// }
+
+// array
+const newArray =[ 
+    "Task 1",
+    "Task 2",
+    "Task 3",
+    "Task 4",
+    "Task 5",
+    "Task 6",
+    "Task 7",
+    "Task 8",
+];
+console.log(newArray)
+
+console.log(newArray.indexOf("Task 5"))
+console.log(newArray.indexOf("Task 3"))
+
+console.log(newArray.includes("Task 3"))
+console.log(newArray.includes("Task 13"))
